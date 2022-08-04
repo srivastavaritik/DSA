a717a40c5a278e2a77f5c29b3ef8b298d339aed9
RITIK KUMAR <78131705+srivastavaritik@users.noreply.github.com>
2022-08-04 09:12:00 +0530

    Time: 6 ms (6.61%), Space: 6 MB (26.45%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.cpp
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int mirrorReflection(int p, int q) {
         int hcf=gcd(p,q);
-        int m=q/hcf; //extension in left and right mirror
-        int n=p/hcf; //extension in top and bottom mirror
+        int m=q/hcf; 
+        int n=p/hcf; 
         if(m%2==0 && n%2==1) return 0;
         if(m%2==1 && n%2==1) return 1;
         if(m%2==1 && n%2==0) return 2;
