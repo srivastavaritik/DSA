04116e4ab14f2e27a299025ef7d2265211032a56
RITIK KUMAR <78131705+srivastavaritik@users.noreply.github.com>
2023-01-22 11:05:08 +0530

    Time: 128 ms (78.39%), Space: 53.5 MB (71.99%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isPalindrome(string str, int s, int e)
+    {
+        while(s<=e)
+        {
+            if(str[s]!=str[e]) return false;
+            s++;
+            e--;
+        }
+        return true;
+    }
+    
+    vector<vector<string>> partition(string s) {
+        vector<vector<string> > ret;
+        if(s.empty()) return ret;
+        
+        vector<string> path;
+        dfs(0, s, path, ret);
+        
+        return ret;
+    }
+    
+    void dfs(int index, string& s, vector<string>& path, vector<vector<string> >& ret) {
+        if(index == s.size()) {
+            ret.push_back(path);
+            return;
+        }
+        for(int i = index; i < s.size(); ++i) {
+            if(isPalindrome(s, index, i)) {
+                path.push_back(s.substr(index, i - index + 1));
+                dfs(i+1, s, path, ret);
+                path.pop_back();
+            }
+        }
+    }
+    
+};
