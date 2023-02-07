dc2281ffb4289032b811573a3646b4b07d4faab2
RITIK KUMAR <78131705+srivastavaritik@users.noreply.github.com>
2023-02-07 12:22:30 +0530

    Time: 112 ms (97.71%), Space: 69.1 MB (86.53%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+      int totalFruit(vector<int> tree) {
+        int res = 0, cur = 0, count_b = 0, a = 0, b = 0;
+        for (int c :  tree) {
+            cur = c == a || c == b ? cur + 1 : count_b + 1;
+            count_b = c == b ? count_b + 1 : 1;
+            if (b != c) a = b, b = c;
+            res = max(res, cur);
+        }
+        return res;
+    }
+};
