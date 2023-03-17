1b912ad1b3d9eb73578031b06bb6ae83b4359fef
RITIK KUMAR <78131705+srivastavaritik@users.noreply.github.com>
2023-03-17 22:33:21 +0530

    Time: 71 ms (52.62%), Space: 34 MB (96.09%) - LeetHub

---
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,35 @@
+class Trie {
+public:
+    Trie() {}
+
+    void insert(string word) {
+        Trie* node = this;
+        for (char ch : word) {
+            if (!node->next.count(ch)) { node->next[ch] = new Trie(); }
+            node = node->next[ch];
+        }
+        node->isword = true;
+    }
+
+    bool search(string word) {
+        Trie* node = this;
+        for (char ch : word) {
+            if (!node->next.count(ch)) { return false; }
+            node = node->next[ch];
+        }
+        return node->isword;
+    }
+
+    bool startsWith(string prefix) {
+        Trie* node = this;
+        for (char ch : prefix) {
+            if (!node->next.count(ch)) { return false; }
+            node = node->next[ch];
+        }
+        return true;
+    }
+
+private:
+    map<char, Trie*> next = {};
+    bool isword = false;
+};
