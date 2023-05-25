896edd77bbe3ff871150d11cb67bb2feaaca75cc
Ritik Kumar <78131705+srivastavaritik@users.noreply.github.com>
2023-05-25 19:30:22 +0530

    Time: 3 ms (72.37%), Space: 7.7 MB (67.00%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -7,9 +7,10 @@ int fun(int i,int j,vector<vector<int>> & grid,vector<vector<int>> & dp){
         if(grid[i][j]==1) return dp[i][j]=0;
         return dp[i][j]=fun(i,j-1,grid,dp)+ fun(i-1,j,grid,dp);
 }
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        if(obstacleGrid[0][0]) return 0;
-        vector<vector<int>> dp(obstacleGrid.size(),vector<int>(obstacleGrid[0].size(),-1));
-       return fun(obstacleGrid.size()-1,obstacleGrid[0].size()-1,obstacleGrid,dp);
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        if(grid[0][0]==1) return 0;
+        int m=grid.size(), n=grid[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+       return fun(m-1,n-1,grid,dp);
     }
 };
