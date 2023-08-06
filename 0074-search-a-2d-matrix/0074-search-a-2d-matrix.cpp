0250bcb7ec6788ece9d80e64870ae3d2b583fce5
Ritik Kumar <78131705+srivastavaritik@users.noreply.github.com>
2023-08-06 20:46:51 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (52.57%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -2,13 +2,25 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m =  matrix.size(), n = matrix[0].size();
-        for(int i=0;i<m;i++)
+        // BRUTE FORCE
+        // for(int i=0;i<m;i++)
+        // {
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         if(matrix[i][j]==target) return 1;
+        //     }
+        // }
+        // return 0;
+        
+        // OPTIMAL
+        int i = 0, j=n-1;
+        while(i<m and j>=0)
         {
-            for(int j=0;j<n;j++)
-            {
-                if(matrix[i][j]==target) return 1;
-            }
+            if(matrix[i][j]==target) return 1;
+            else if(matrix[i][j] < target) i++;
+            else j--;
         }
         return 0;
+        
     }
 };
