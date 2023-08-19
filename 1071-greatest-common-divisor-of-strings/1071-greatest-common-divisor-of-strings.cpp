81b816ea90cbc5075bbbf090ce4c370327f2088c
Ritik Kumar <78131705+srivastavaritik@users.noreply.github.com>
2023-08-19 15:33:58 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (59.59%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
-    string gcdOfStrings(const string& s1, const string& s2)
-    {
-        return (s1 + s2 == s2 + s1)  
-		    ? s1.substr(0, gcd(size(s1), size(s2)))
-			: "";
+    string gcdOfStrings(string str1, string str2) {
+        return (str1 + str2 == str2 + str1) ? str1.substr(0,__gcd(str1.size(), str2.size())):"";
     }
 };
