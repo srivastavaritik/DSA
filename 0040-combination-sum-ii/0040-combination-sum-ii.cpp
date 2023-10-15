34ef8cde0a24e747bd7da414d6d89f17745f7e56
Ritik Kumar <78131705+srivastavaritik@users.noreply.github.com>
2023-10-15 13:26:48 +0530

    Time: 7 ms (39.82%), Space: 11.5 MB (24.32%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -2,26 +2,40 @@ class Solution {
 public:
     void solve(vector<int> &vec, int target, int ind, set<vector<int>>&st, vector<int> &temp)
     {
-        //base
-        if(ind==vec.size())
+//         //base
+//         if(ind==vec.size())
+//         {
+//             if(target==0)
+//             {
+//                 st.insert(temp);
+//             }
+//             return;
+//         }
+//         //pick
+//         if(vec[ind]<=target)
+//         {
+//             temp.push_back(vec[ind]);
+//             solve(vec, target-vec[ind], ind+1, st, temp);
+//             //not pick
+//             temp.pop_back();
+//         }
+        
+//         while(ind+1<vec.size() and vec[ind]==vec[ind+1]) ind++;
+//         solve(vec, target, ind+1, st, temp);
+        
+        if(target==0)
         {
-            if(target==0)
-            {
-                st.insert(temp);
-            }
+            st.insert(temp);
             return;
         }
-        //pick
-        if(vec[ind]<=target)
+        for(int i=ind;i<vec.size();i++)
         {
-            temp.push_back(vec[ind]);
-            solve(vec, target-vec[ind], ind+1, st, temp);
-            //not pick
+            if(i>ind and vec[i]==vec[i-1]) continue;
+            if(vec[i]>target) break;
+            temp.push_back(vec[i]);
+            solve(vec, target-vec[i], i+1, st, temp);
             temp.pop_back();
         }
-        
-        while(ind+1<vec.size() and vec[ind]==vec[ind+1]) ind++;
-        solve(vec, target, ind+1, st, temp);
         
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
