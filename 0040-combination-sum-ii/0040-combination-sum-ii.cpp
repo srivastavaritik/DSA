0faaeb025a19e64285cbfbe7c3a6b8ad85205763
Ritik Kumar <78131705+srivastavaritik@users.noreply.github.com>
2023-10-16 21:08:51 +0530

    Time: 3 ms (83.59%), Space: 11.4 MB (24.58%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,48 +1,29 @@
 class Solution {
 public:
-    void solve(vector<int> &vec, int target, int ind, set<vector<int>>&st, vector<int> &temp)
+    void solve(int ind, vector<int> &vec, int target, set<vector<int>>&st, vector<int>&temp)
     {
-//         //base
-//         if(ind==vec.size())
-//         {
-//             if(target==0)
-//             {
-//                 st.insert(temp);
-//             }
-//             return;
-//         }
-//         //pick
-//         if(vec[ind]<=target)
-//         {
-//             temp.push_back(vec[ind]);
-//             solve(vec, target-vec[ind], ind+1, st, temp);
-//             //not pick
-//             temp.pop_back();
-//         }
-        
-//         while(ind+1<vec.size() and vec[ind]==vec[ind+1]) ind++;
-//         solve(vec, target, ind+1, st, temp);
-        
-        if(target==0)
+        if(ind==vec.size())
         {
-            st.insert(temp);
+            if(target==0)
+            {
+                st.insert(temp);
+            }
             return;
         }
-        for(int i=ind;i<vec.size();i++)
+        if(vec[ind]<=target)
         {
-            if(i>ind and vec[i]==vec[i-1]) continue;
-            if(vec[i]>target) break;
-            temp.push_back(vec[i]);
-            solve(vec, target-vec[i], i+1, st, temp);
+            temp.push_back(vec[ind]);
+            solve(ind+1, vec, target-vec[ind], st, temp);
             temp.pop_back();
         }
-        
+        while(ind<vec.size()-1 and vec[ind]==vec[ind+1]) ind++;
+        solve(ind+1, vec, target, st, temp);
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        set<vector<int>> st;
+        set<vector<int>>st;
         vector<int> temp;
         sort(candidates.begin(), candidates.end());
-        solve(candidates, target, 0, st, temp);
+        solve(0, candidates, target, st, temp);
         vector<vector<int>> ans(st.begin(), st.end());
         return ans;
     }
